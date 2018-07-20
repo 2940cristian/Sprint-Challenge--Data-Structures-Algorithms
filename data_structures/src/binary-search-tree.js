8df41a2fdd43d039cb1cec29c0a74191f2f9d14f
2940cristian <2940cristian@gmail.com>
2018-07-19 21:22:03 -0700

    Implemented binary search tree

---
+++ data_structures/src/binary-search-tree.js
@@ -0,0 +1,97 @@
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  depthFirstForEach(cb) {
+    /* Your code here */
+
+    cb(this.value);
+
+
+    if(this.left) {
+      this.left.depthFirstForEach(cb);
+    }
+
+    if(this.right) {
+      this.right.depthFirstForEach(cb)
+    }
+    
+  }
+
+  breadthFirstForEach(cb) {
+    /* Your code here */
+   const q = [];
+
+   q.push(this)
+
+   while(q.length) {
+     const currentNode = q.shift();
+
+     if(currentNode.left) {
+       q.push(currentNode.left)
+     }
+     
+
+     if(currentNode.right) {
+       q.push(currentNode.right)
+     }
+
+     cb(currentNode.value)
+   }
+  }
+
+  insert(value) {
+    const newNode = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = newNode;
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value >= this.value) {
+      if (!this.right) {
+        this.right = newNode;
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+
+  contains(target) {
+    if (this.value === target) {
+      return true;
+    }
+    if (this.left) {
+      if (this.left.contains(target)) {
+        return true;
+      }
+    }
+    if (this.right) {
+      if (this.right.contains(target)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getMax() {
+    if (!this) return null;
+
+    let max = this.value;
+    let current = this;
+
+    while (current) {
+      if (current.value > max) {
+        max = current.value;
+      }
+      current = current.right;
+    }
+
+    return max;
+  }
+}
+
+module.exports = BinarySearchTree;
